a7d877144d6e4ba40ede67f6ded8697c126d1b84
Eric Maya <ericmaya8a@gmail.com>
2020-05-04 17:39:24 -0500

    Updates

---
+++ precache-manifest.5ed110e60272fa498cc5078bd4b4b47b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afbc0813fe159f84eaae218617559ca",
+    "url": "/pages-test/index.html"
+  },
+  {
+    "revision": "e552c140a5f3314efd58",
+    "url": "/pages-test/static/css/main.70fd3448.chunk.css"
+  },
+  {
+    "revision": "3ff8209ee0702e31285b",
+    "url": "/pages-test/static/js/2.37259395.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pages-test/static/js/2.37259395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e552c140a5f3314efd58",
+    "url": "/pages-test/static/js/main.2e6b86de.chunk.js"
+  },
+  {
+    "revision": "a8fd6f93d3081eb0f14e",
+    "url": "/pages-test/static/js/runtime-main.39b1ab98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
