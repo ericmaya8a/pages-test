d79f2eb6467b04fbf5616ec2bce2a885eb33642f
Eric Maya <ericmaya8a@gmail.com>
2020-05-07 09:59:09 -0500

    Refactoring Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink exact to="/dashboard">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink exact to="/about">
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink exact to="/users">
-          Users
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/dashboard">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/about">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/users">
+            Users
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Navigation;
