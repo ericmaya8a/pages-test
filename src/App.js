af013a1882c135eb6a5d9c2e873ea92ec48dcb48
Eric Maya <ericmaya8a@gmail.com>
2020-05-08 11:50:31 -0500

    Refactorig Navigation
    Refactoring App

--- src/App.js
+++ src/App.js
@@ -6,28 +6,26 @@ import Users from './Users';
 import Navigation from './Navigation';
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Navigation />
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/dashboard" />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/users">
-            <Users />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <div className="App">
+      <Navigation />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/users">
+          <Users />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function Navigation() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink exact to="/dashboard">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/about">
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/users">
-            Users
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink exact to="/dashboard">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/about">
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/users">
+          Users
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navigation;
